5a57f18a760a511cc5fc1aa3797346f5970df557
hello <ssaravananss077@gmail.com>
2026-08-21 12:50:40 +0530

    recent sothapal

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-console.log("hello mom and dad");
-
-for(var i=0;i<5;i++){
-    console.timeLog("code");
-=======
 console.log("welcome to playlist");
 for(var i=0;i<5;i++){
     console.log("pro coder");  
->>>>>>> main
 }
